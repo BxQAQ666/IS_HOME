8ab989a1ff9f31ef05580d4700a467d7663ca88f
BxQAQ666 <161921697+BxQAQ666@users.noreply.github.com>
2024-11-11 13:56:14 +0800

    Update DES.java

--- DES.java
+++ DES.java
@@ -5,8 +5,12 @@ import java.util.Random;
 
 public class DES {
     public static void main(String[] args) {
+        create();
+    }
+
+    public static void create(){
         ArrayList<int[]> list;
-        list = CreateTest();
+        list = CreateTest_Plaintext();
         for(int i = 0; i < list.size(); i++){
             if(i % 2 == 0)
                 System.out.println("i = " + (i / 2 + 1));
@@ -19,7 +23,7 @@ public class DES {
         }
     }
 
-    public static ArrayList<int[]> CreateTest(){
+    public static ArrayList<int[]> CreateTest_Plaintext(){
         ArrayList<int[]> list = new ArrayList<>();
         Random rand = new Random();
         for(int i = 1 ; i <= 64 ; ++i)
@@ -28,6 +32,44 @@ public class DES {
             for(int j = 0 ; j < 64 ; ++j)
             {
                 arr[j] = rand.nextInt(2);
+            }
+            list.add(arr);
+            int[] newarr = arr.clone();
+            boolean[] vis = new boolean[64];
+            for(int j = 0 ; j < i ; ++j)
+            {
+                int temp = rand.nextInt(64);
+                while(vis[temp])
+                {
+                    temp = rand.nextInt(64);
+                }
+                vis[temp] = true;
+                newarr[temp] ^= 1;
+            }
+            list.add(newarr);
+        }
+        return list;
+    }
+
+    public static ArrayList<int[]> CreateTest_Key(){
+        ArrayList<int[]> list = new ArrayList<>();
+        Random rand = new Random();
+        for(int i = 1 ; i <= 64 ; ++i)
+        {
+            int[] arr = new int[64];
+            for(int j = 0 ; j < 8 ; ++j)
+            {
+                int cnt = 0;
+                for(int k = 0 ; k < 7 ; ++k)
+                {
+                    arr[j * 8 + k] = rand.nextInt(2);
+                    if(arr[j * 8 + k] == 1)
+                    ++cnt;
+                }
+                if(cnt % 2 == 0)
+                arr[j * 8 + 7] = 1;
+                else
+                arr[j * 8 + 7] = 0; 
             }
             list.add(arr);
             int[] newarr = arr.clone();
